b2cc5a37bb9dfe17d72f0c662a54965723d01ac8
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-09-26 12:57:49 +0300

    completed assignment

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,21 @@
-import React from "react";
-
-function Search() {
-  return (
-    <div className="ui large fluid icon input">
-      <input
-        type="text"
-        placeholder="Search your Recent Transactions"
-        onChange={() => console.log("Searching...")}
-      />
-      <i className="circular search link icon"></i>
-    </div>
-  );
+import React, { useState } from "react";
+function Search({ searchingFn }) {
+	const [search, setSearch] = useState("");
+    //state variable, setter function
+	function handleChange(e) {
+		setSearch(e.target.value);
+		searchingFn(search);
+	}
+	return (
+		<div className="ui large fluid icon input">
+			<input
+				value={search}
+				type="text"
+				placeholder="Search your Recent Transactions"
+				onChange={handleChange}
+			/>
+			<i className="circular search link icon"></i>
+		</div>
+	);
 }
-
 export default Search;
