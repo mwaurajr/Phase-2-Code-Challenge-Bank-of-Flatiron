70694fe22696dc88be9235354c5a70ceb48df2cd
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-09-26 12:58:12 +0300

    completed assignment

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,21 +1,5 @@
 import React from "react";
-// import React,{useEffect,useState} from "react";
-// function Transaction() {
-//   const [transactions,setAddTransactions]= useState([]);
-//   useEffect(()=>{
-//   getTransactions();
-//    },[])
-//   function getTransactions(){
-//     fetch("http://localhost:8001/transactions")
-//     .then((response)=>response.json())
-//     .then((data)=>{
-//       console.log(data)
-//       setAddTransactions(data) 
-//   })
-//   .catch((err)=>{
-//     console.log(err);
-//   });
-// }
+
 
 
 function Transaction({ transactionsData }) {
